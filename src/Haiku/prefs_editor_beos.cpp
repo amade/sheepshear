80698f1738073af7d0a377ac060969c19b2fa665
Alexander von Gluck IV <kallisti5@unixzen.com>
2012-06-16 05:21:07 +0000

    Haiku: Add missing Haiku API headers

--- src/Haiku/prefs_editor_beos.cpp
+++ src/Haiku/prefs_editor_beos.cpp
@@ -2,6 +2,7 @@
  *  prefs_editor_beos.cpp - Preferences editor, BeOS implementation
  *
  *  SheepShaver (C) 1997-2008 Christian Bauer and Marc Hellwig
+ *  SheepShear (C) 2012 Alexander von Gluck
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,7 +18,6 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 #include <SerialPort.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,6 +32,28 @@
 #include "xpram.h"
 #include "about_window.h"
 #include "user_strings.h"
+
+#include <app/Application.h>
+#include <interface/Button.h>
+#include <interface/CheckBox.h>
+#include <interface/ListItem.h>
+#include <interface/ListView.h>
+#include <interface/MenuBar.h>
+#include <interface/MenuField.h>
+#include <interface/MenuItem.h>
+#include <interface/PopUpMenu.h>
+#include <interface/ScrollView.h>
+#include <interface/Slider.h>
+#include <interface/StringView.h>
+#include <interface/TabView.h>
+#include <interface/TextView.h>
+#include <interface/TextControl.h>
+#include <interface/View.h>
+#include <interface/Window.h>
+#include <storage/Entry.h>
+#include <storage/FilePanel.h>
+#include <storage/Path.h>
+#include <support/SupportDefs.h>
 
 
 // Special colors
